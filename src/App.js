492ce0857f8621cd0208c7c68d1369e969653404
RYU <96900106+RyuKiyo66@users.noreply.github.com>
2022-05-14 20:52:14 +0900

    ColorfulMessage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
+  const contentLadyStyle = {
+    color: "pink",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1>Hello</h1>
-      <p>Hi!</p>
+      <h1 style={{ color: "red" }}>Hello</h1>
+      <p style={contentStyle}>How are you?</p>
+      <p style={contentLadyStyle}>How are you?</p>
+      <button onClick={onClickButton}>BUTTON</button>
     </>
   );
 };
---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const colorfulMessage = () => alert();
+const contentLadyStyle = {
+  color: 'pink',
+  fontSize: '18px'
+}
+
+return (
+  <>
+    <h1 style={{ color: "red" }}>Hello</h1>
+    <p style={contentStyle}>How are you?</p>
+    <p style={contentLadyStyle}>I am fine, thank you.</p>
+    <button onClick={onClickButton}>BUTTON</button>
+  </>
+);
+};
+
+export default colorfulMessage;
